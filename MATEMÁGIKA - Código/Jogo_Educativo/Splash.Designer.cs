5c3c82dfb04c7880e88892bb5a17e8e2131b8e6d
LucaDillenburg <LucaDillenburg@users.noreply.github.com>
2018-05-24 19:34:02 -0300

    Matemagika
    Jogo bem debilitado e multiplayer ainda não funcionando

---
+++ MATEMÁGIKA - Código/Jogo_Educativo/Splash.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Jogo_Educativo
+{
+    partial class Splash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pgr = new System.Windows.Forms.ProgressBar();
+            this.tmrTempo = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pgr
+            // 
+            this.pgr.Location = new System.Drawing.Point(188, 204);
+            this.pgr.MarqueeAnimationSpeed = 0;
+            this.pgr.Maximum = 200;
+            this.pgr.Name = "pgr";
+            this.pgr.Size = new System.Drawing.Size(514, 57);
+            this.pgr.Step = 0;
+            this.pgr.TabIndex = 0;
+            // 
+            // tmrTempo
+            // 
+            this.tmrTempo.Enabled = true;
+            this.tmrTempo.Interval = 1;
+            this.tmrTempo.Tick += new System.EventHandler(this.tmrTempo_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Stencil", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(227, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(451, 76);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MATEMÁGIKA";
+            this.label1.Visible = false;
+            // 
+            // pic
+            // 
+            this.pic.BackColor = System.Drawing.Color.Transparent;
+            this.pic.Location = new System.Drawing.Point(-2, 373);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(76, 99);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic.TabIndex = 2;
+            this.pic.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Jogo_Educativo.Properties.Resources.matemagika;
+            this.pictureBox1.Location = new System.Drawing.Point(121, 103);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(676, 94);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Splash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Jogo_Educativo.Properties.Resources.Wallpaper1;
+            this.ClientSize = new System.Drawing.Size(880, 530);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pgr);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Splash";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Splash";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Splash_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar pgr;
+        private System.Windows.Forms.Timer tmrTempo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
